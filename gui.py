c6663856157eaca4599b4e19efe5bc8b6a2ea0a6
Carsten Thue-Bludworth <carstentb@protonmail.com>
2022-05-01 23:18:31 -0400

    Fixed to follow cleanup branch of unigui and use the new colorscheme calling convention

--- gui.py
+++ gui.py
@@ -1,19 +1,20 @@
 from unigui import UniGui, PygameDisplay
-from unigui import TextWidget
+from unigui import TextWidget, Solarized
 
 # Global properties of our GUI
 WIDTH = 800
 HEIGHT = 600
 SCALE_FACTOR = 2
+CS = Solarized.light
 
 # Create the display
 display = PygameDisplay(WIDTH*SCALE_FACTOR, HEIGHT*SCALE_FACTOR)
 
 # Create the GUI
-gui = UniGui(WIDTH, HEIGHT, scale=SCALE_FACTOR)
+gui = UniGui(WIDTH, HEIGHT, scale=SCALE_FACTOR, colorscheme=Solarized.light)
 
 # Create our widgets
-tw = TextWidget("text", 0, 0, WIDTH, HEIGHT)
+tw = TextWidget("text", 0, 0, WIDTH, HEIGHT, colorscheme=Solarized.light)
 tw.set_value("hello from radar app", h_justification="center", v_justification="top")
 gui.add_widget(tw)
 
