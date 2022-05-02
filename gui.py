2272de34f466ee5d6125d4d372e800d564276004
Carsten Thue-Bludworth <carstentb@protonmail.com>
2022-05-01 23:38:10 -0400

    Added an asset screenshot

--- gui.py
+++ gui.py
@@ -2,19 +2,19 @@ from unigui import UniGui, PygameDisplay
 from unigui import TextWidget, Solarized
 
 # Global properties of our GUI
-WIDTH = 800
-HEIGHT = 600
-SCALE_FACTOR = 2
-CS = Solarized.light
+WIDTH = 1024
+HEIGHT = 1024
+SCALE_FACTOR = 1
+CS = Solarized.dark
 
 # Create the display
 display = PygameDisplay(WIDTH*SCALE_FACTOR, HEIGHT*SCALE_FACTOR)
 
 # Create the GUI
-gui = UniGui(WIDTH, HEIGHT, scale=SCALE_FACTOR, colorscheme=Solarized.light)
+gui = UniGui(WIDTH, HEIGHT, scale=SCALE_FACTOR, colorscheme=CS)
 
 # Create our widgets
-tw = TextWidget("text", 0, 0, WIDTH, HEIGHT, colorscheme=Solarized.light)
+tw = TextWidget("text", 0, 0, WIDTH, HEIGHT, colorscheme=CS)
 tw.set_value("hello from radar app", h_justification="center", v_justification="top")
 gui.add_widget(tw)
 
