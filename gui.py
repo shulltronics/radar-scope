8aca8c4c9fb4a913d0e93052aada3eab73a86793
Thue-bludworth <carstentb@ufl.edu>
2022-05-02 14:06:49 -0400

    Incorporated unigui upgrades, got a simple background image loaded with TextWidget on top

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
 from unigui import UniGui, PygameDisplay
-from unigui import TextWidget, Solarized
+from unigui import Widget, TextWidget, Solarized
+import adafruit_imageload
+import displayio
 
 # Global properties of our GUI
-WIDTH = 1024
-HEIGHT = 1024
+WIDTH = 800
+HEIGHT = 800
 SCALE_FACTOR = 1
 CS = Solarized.dark
 
@@ -13,8 +15,34 @@ display = PygameDisplay(WIDTH*SCALE_FACTOR, HEIGHT*SCALE_FACTOR)
 # Create the GUI
 gui = UniGui(WIDTH, HEIGHT, scale=SCALE_FACTOR, colorscheme=CS)
 
+class ImageWidget(Widget):
+    """
+    A widget that loads and displays a bmp image
+    """
+    def __init__(self):
+        image, pal = adafruit_imageload.load(
+            "assets/android-screenshot.bmp",
+            bitmap=displayio.Bitmap,
+            palette=displayio.Palette,
+        )
+        super().__init__("image", 0, 0, image.width, image.height, colorscheme=CS)
+        print("palette length: ", pal.__len__())
+        self.tg = displayio.TileGrid(
+            image,
+            pixel_shader=pal,
+            width=1,
+            height=1,
+            tile_width=800,
+            tile_height=800,
+        )
+        self.append(self.tg)
+
+i = ImageWidget()
+gui.add_widget(i)
+
 # Create our widgets
 tw = TextWidget("text", 0, 0, WIDTH, HEIGHT, colorscheme=CS)
+tw.set_bg_transparent(True)
 tw.set_value("hello from radar app", h_justification="center", v_justification="top")
 gui.add_widget(tw)
 
